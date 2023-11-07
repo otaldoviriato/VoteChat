2f28847310b2690c12d5d4afcfe99ef0858cd22c
Laura Riberto <lahriberto@gmail.com>
2023-11-07 16:02:06 -0300

    feat: "configs"

--- React-Native-App/App.js
+++ React-Native-App/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import Nav from './Navigator';
+import React from 'react'
+import Nav from './Navigator'
 import AuthProvider from './context/authContext'
 
 export default function App() {
+  const socket = SocketIOClient('http://localhost:3000')
+
+  socket.on('connect', () => {
+    console.log('Connected to server')
+  })
+  
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server')
+  })
+  
+  socket.on('chat message', (msg) => {
+    console.log('New message:', msg)
+  })
+  
+  const sendMessage = (msg) => {
+    socket.emit('chat message', msg)
+  }
+
   return (
     <AuthProvider>
       <Nav />
     </AuthProvider>
-  );
+  )
 }
