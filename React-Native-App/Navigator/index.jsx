9b5214b3c4e234d496255e092f07f7894de23ffc
Laura Riberto <lahriberto@gmail.com>
2023-11-13 18:02:44 -0300

    merging branch 'main'

--- React-Native-App/Navigator/index.jsx
+++ React-Native-App/Navigator/index.jsx
@@ -1,32 +1,12 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { MaterialIcons } from '@expo/vector-icons'
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-// Importe os componentes de tela que você deseja navegar
-import ListRoomsScreen from '../screens/ListRoomsScreen'
-import RoomDetailsScreen from '../screens/RoomDetailsScreen'
-import ListPendingScreen from '../screens/ListPendingScreen'
-
-
-const Tab = createMaterialTopTabNavigator();
+import StackNavigator from './components/StackNavigator'
 
 function NavigatorComponent() {
 
   return (
     <NavigationContainer independent={true}>
-      <StackNaigator />
-      {/*<Tab.Navigator initialRouteName="Grupos" tabBarPosition="bottom">
-
-         <Tab.Screen name="Grupos" component={RoomsStack} options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="home" size={24} color={color} />
-          )
-        }} /> 
-
-      </Tab.Navigator>*/}
-      <NewRoomButton />
+      <StackNavigator />
     </NavigationContainer>
   )
 }
