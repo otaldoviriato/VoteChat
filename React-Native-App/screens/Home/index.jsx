a88183e5055da14359352c56020a09c40acafb87
Laura Riberto <lahriberto@gmail.com>
2023-11-14 17:37:03 -0300

    feat: "creating 'createRoom' temporary component"

--- React-Native-App/screens/Home/index.jsx
+++ React-Native-App/screens/Home/index.jsx
@@ -3,7 +3,11 @@ import { View, StyleSheet, BackHandler, Alert, Button } from 'react-native'
 import RoomsList from './components/RoomsList'
 import NewRoomButton from './components/NewRoomButton'
 import { useNavigation } from '@react-navigation/native'
-import EnterRoom from './components/EnterRoom'
+import CreateRoomLink from './components/CreateRoomLink'
+import Temporary from './components/temporary'
+import RegisterForm from '../Login/components/register-form'
+import Logout from './components/logout'
+
 
 export default function ListRoomsScreen() {
   useEffect(() => {
@@ -37,8 +41,10 @@ export default function ListRoomsScreen() {
   <>
     <View style={styles.container}>
       <RoomsList />
+      <Logout/>
       <NewRoomButton />
-      <EnterRoom/>
+      <CreateRoomLink/>
+      <Temporary/>
       <Button onPress={handlePress} title='ver requisitos'/>
     </View>
   </>
