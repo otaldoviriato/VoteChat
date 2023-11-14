4423c38972eb93915e3ab5936b21fd4aa9e4e5d6
Laura Riberto <lahriberto@gmail.com>
2023-11-14 17:34:20 -0300

    feat: "creating newRoom button and logic"

--- React-Native-App/screens/Home/components/NewRoomButton/index.jsx
+++ React-Native-App/screens/Home/components/NewRoomButton/index.jsx
@@ -1,27 +1,144 @@
-import { useState, React, useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Entypo } from '@expo/vector-icons';
-import { AuthContext } from '../../../../context/authContext'
+import React, { useState, useContext } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { AuthContext } from "../../../../context/authContext"
+import { View, StyleSheet, TouchableOpacity, Modal, TextInput, Text } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
 
-const NewRoomButton = () => {
-  const { setUser } = useContext(AuthContext)
+export default function NewRoomButton() {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [roomName, setRoomName] = useState('')
+  const [roomDescription, setRoomDescription] = useState('')
+  const [error, setError] = useState('')
+  const [currentPage, setCurrentPage] = useState(1)
 
-  async function deslogar () {
-    await AsyncStorage.removeItem('user1')
-    setUser(null)
+  const { user } = useContext(AuthContext)
+
+  const navigation = useNavigation()
+
+  function openModal() {
+    setCurrentPage(1);
+    setModalVisible(true);
+  }
+
+  async function createRoom() {
+    const id = user?._id
+
+    try {
+
+      const res = await fetch("http://192.168.100.5:3000/api/homeScreenAPI/createRoom", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          roomName,
+          id, 
+          roomDescription
+        }),
+      })
+
+      if (res.ok) {
+        setRoomName('')
+        setRoomDescription('')
+        closeModal()
+        const responseData = await res.json()
+        const data = responseData.data
+
+        //lógica para redireconar para a sala
+        navigation.navigate('DetalhesDaSala', { data })
+
+      } else {
+        setRoomName('')
+        setRoomDescription('')
+        console.log("Registro da sala falhou.")
+      }
+
+    } catch (error) {
+      console.log('error during registraion', error)
+    }
+  }
+
+  function goToNextPage() {
+    if (!roomName) {
+      setError("Preencha todos os campos")
+      return
+    }
+    // Lógica para lidar com a transição para a próxima página
+    setCurrentPage(currentPage + 1)
+  }
+
+  function closeModal() {
+    setModalVisible(false)
+    setRoomName('')
+    setRoomDescription('')
   }
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={deslogar} style={styles.button}>
-        <Entypo name="plus" size={42} color="white" />
-      </TouchableOpacity>
-    </View>
-  )
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={openModal} style={styles.button}>
+          <Entypo name="plus" size={42} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            {currentPage === 1 && (
+              <>
+                <Text>Qual o nome da sala ?</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Nome da Sala"
+                  onChangeText={(text) => setRoomName(text)}
+                  value={roomName}
+                />
+                <TouchableOpacity onPress={goToNextPage}>
+                  <Text>Próxima</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={closeModal}>
+                  <Text>Cancelar</Text>
+                </TouchableOpacity>
+                <Text>{error}</Text>
+              </>
+            )}
+            {currentPage === 2 && (
+              <>
+                <Text>Agora explique quais coisas são relevantes para que um novo integrante seja aceito na sala (opcional):</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Requisitos para entrar na sala"
+                  onChangeText={(text) => setRoomDescription(text)}
+                  value={roomDescription}
+                />
+                <TouchableOpacity onPress={closeModal}>
+                  <Text>Cancelar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={createRoom}>
+                  <Text>Criar Sala</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   button: {
     position: 'absolute',
     right: 30,
@@ -33,6 +150,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#edc618',
     borderRadius: 100,
   },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    marginTop: 10,
+    padding: 10,
+  },
 });
-
-export default NewRoomButton;
