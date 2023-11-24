d09084eb3661fadd5148b869e765f7af125b28f6
Laura Riberto <lahriberto@gmail.com>
2023-11-24 16:14:48 -0300

    feat: "requirements screen"

--- React-Native-App/screens/RoomRequirements/index.js
+++ React-Native-App/screens/RoomRequirements/index.js
@@ -1,55 +1,82 @@
 import React from 'react';
 import { AuthContext } from "../../context/authContext"
 import { useContext, useState } from "react"
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Button, Image } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import axios from 'axios'
 
 
 export default function RoomRequirementsScreen({ route }) {
-    const { user } = useContext(AuthContext)
-    const [mensagem, setMensagem] = useState('')
+  const { user } = useContext(AuthContext)
+  const [mensagem, setMensagem] = useState('')
+  const [answer, setAnswer] = useState('')
+  const [selectedImage, setSelectedImage] = useState(null)
 
-    const handleSubmit = async () => {
-        console.log('ok')
+  const handleImagePicker = async () => {
+    try {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access media library was denied');
+        return;
+      }
 
-        try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
 
-            const res = await fetch("http://192.168.100.5:3000/api/ScreenAPI/enterRoom", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    'Authorization': `${user.trim() || ''}`
-                },
-                body: JSON.stringify({
-                    id_sala: idSala,
-                    pedidoEm: new Date()
-                }),
-            })
+      if (!result.canceled) {
+        setSelectedImage(result.assets[0].uri);
+        console.log(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.error('Error picking an image:', error);
+    }
+  }
 
-            if (res.status == 200) {
-                setMensagem("Solicitação enviado para o grupo com sucesso, o grupo tem até 7 dias para votar!")
-            }
-            else if (res.status == 400) {
-                console.log('sala não encontrada')
-            }
-            else if (res.status == 401) {
-                setMensagem('usuário já é membro ou está pendente na sala')
-                console.log('usuário já é membro ou está pendente na sala')
-            } else {
-                console.log("Registro do usuário falhou.", JSON.stringify(res))
-            }
 
-        } catch (error) {
-            console.error('Erro durante a entrada na sala:', error)
-        }
-
+  //Requisição para Incluir um usuário como pendente em uma sala
+  const sendRequirements = async () => {
+    const url = "http://192.168.100.5:3000/api/ScreenAPI/enterRoom"
+    const headers = {
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `${user || ''}`
+      }
+    }
+    const body = {
+      id_sala: route.params?.id,
+      pedidoEm: new Date(),
+      answer: answer,
+      picture: selectedImage
     }
 
-    return (
-        <>
-            <View>
-                <Text>Entrar na Sala: {route.params?.id}</Text>
-                <Text>{mensagem}</Text>
-            </View>
-        </>
-    )
+    await axios.post(url, body, headers)
+      .then((res) => {
+
+      }).catch(function (error) { console.log(error) })
+  }
+
+  return (
+    <>
+      <View>
+        <Text>Entrar na Sala: {route.params?.id}</Text>
+        <TextInput
+          placeholder="Digite alguma coisa"
+          value={answer}
+          onChangeText={(text) => setAnswer(text)}
+        />
+        <Text></Text>
+        <TouchableOpacity onPress={handleImagePicker}>
+          <Text>Selecionar Imagem</Text>
+        </TouchableOpacity>
+        {selectedImage && <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200 }} />}
+        <Text></Text>
+        <Button title="Enviar" onPress={sendRequirements} />
+        <Text>{mensagem}</Text>
+      </View>
+    </>
+  )
 }
