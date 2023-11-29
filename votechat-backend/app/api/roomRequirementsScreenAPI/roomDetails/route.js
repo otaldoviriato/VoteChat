1d6953f224980150a34b26f118341bc97fa59b8e
Laura Riberto <lahriberto@gmail.com>
2023-11-29 11:48:15 -0300

    fix: "roomDataAPI"

--- votechat-backend/app/api/roomRequirementsScreenAPI/roomDetails/route.js
+++ votechat-backend/app/api/roomRequirementsScreenAPI/roomDetails/route.js
@@ -1,21 +1,21 @@
 import { connectMongoDB } from "../../../../lib/mongodb"
 import Salas from "../../../../models/salas"
 import { NextResponse } from "next/server"
-import verifyToken from "../../verifyTokenFunction"
+import jwt from 'jsonwebtoken'
 
 export async function POST(req) {
     try {
         const { id_sala } = await req.json()
 
         //Descriptografa o Token
-        const DecryptedToken = await verifyToken(req.headers.get('authorization'))
+        const id_user = jwt.verify(req.headers.get('authorization'), process.env.SECRET_KEY).id_user
 
         //Busca os dados no MongoDB
         await connectMongoDB()
         const sala = await Salas.findById(id_sala)
 
         //Devolve as informações como resposta da requisição
-        return NextResponse.json({ sala })
+        return NextResponse.json( sala )
     } catch (error) {
         console.log(error)
         return NextResponse.json({ message: error, success: false });
