8a58315f506653df99290999b81f38f4d46bc2ca
Laura Riberto <lahriberto@gmail.com>
2023-11-29 11:48:50 -0300

    feat: "storage logic"

--- React-Native-App/context/authContext.jsx
+++ React-Native-App/context/authContext.jsx
@@ -3,31 +3,36 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const AuthContext = createContext()
 
-function AuthProvider( {children} ){
-    const [user, setUser] = useState()
-    const [roomData, setRoomData] = useState([])
+function AuthProvider({ children }) {
+  const [user, setUser] = useState({token: null, name: null, email: null, profilePicture: null })
+  const [roomData, setRoomData] = useState([])
 
-    //Atualiza a state user com o id do usuário caso o mesmo já esteja logado.
-    useEffect(() => {
-        const fetchUser = async () => {
-          const storedUser = await AsyncStorage.getItem('user1');
-          if(storedUser){setUser(storedUser)}
+  //Atualiza a state user com o token do usuário caso o mesmo já esteja logado.
+  useEffect(() => {
+    const fetchUser = async () => {
+      const storedUser = await AsyncStorage.getItem('user1')
+      if (storedUser) {
+        setUser(storedUser)
+      }
 
-          console.log("Esse é o token: "+storedUser)
-        };
-      
-        fetchUser();
+      console.log("Esse é o token: " + storedUser)
+    }
 
-        
-      }, []);
+    fetchUser()
+  }, [])
 
-  
+  useEffect(() => {
+    const fetchUser = async () => {
+      await AsyncStorage.setItem('user1', user)
+    }
+    fetchUser()
+  }, [user])
 
-    return(
-        <AuthContext.Provider value={{user, setUser, roomData, setRoomData}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  return (
+    <AuthContext.Provider value={{ user, setUser, roomData, setRoomData }}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
 export default AuthProvider
