82522d8334ac0436ccf0e72975cbfa0de60fa53a
Laura Riberto <lahriberto@gmail.com>
2023-11-29 13:55:35 -0300

    feat: "update user API"

--- votechat-backend/app/api/userProfileScreenAPI/updateUser/route.js
+++ votechat-backend/app/api/userProfileScreenAPI/updateUser/route.js
@@ -1,21 +1,21 @@
 import { connectMongoDB } from "../../../../lib/mongodb"
 import User from "../../../../models/user"
 import { NextResponse } from "next/server"
-import verifyToken from "../../verifyTokenFunction"
+import jwt from 'jsonwebtoken'
 
 export async function POST(req) {
     let id_user
-    let newUserToken 
+    let newUserToken
 
     try {
         //Verfica se recebeu o token e decide se decodifica ou cria um novo usuário
-        if(req.headers.get('authorization')){
+        if (req.headers.get('authorization')) {
             // Decodifica o token
             id_user = jwt.verify(req.headers.get('authorization'), process.env.SECRET_KEY).id_user
         } else {
             const createdUser = await User.create({})
             id_user = createdUser._id
-            
+
             // Se o token não existe, cria um novo
             newUserToken = jwt.sign({ id_user }, process.env.SECRET_KEY)
         }
@@ -26,15 +26,17 @@ export async function POST(req) {
 
         // Update the user fields
         const data = await req.json()
-        if (data.name) user.name = data.name
-        if (data.email) user.email = data.email
+
+        if (data.newName) user.name = data.newName
+        if (data.newEmail) user.email = data.newEmail
+
         // Fazer lógica para salvar path no DB e foto no servidor
         if (data.profilePicture) user.profilePicture = data.profilePicture
 
         // Save the updated user
         const updatedUser = await user.save()
 
-        return NextResponse.json(updatedUser)
+        return NextResponse.json({ name: updatedUser.name, email: updatedUser.email, profilePicture: updatedUser.profilePicture })
     } catch (error) {
         console.error('Error:', error)
         return NextResponse.json({ message: error.message || "Internal Server Error", success: false })
