ceae5d4aecc4773ec23513f2496c3b97559db246
Laura Riberto <lahriberto@gmail.com>
2023-11-29 15:12:10 -0300

    fix: "verifyTokenFunction"

--- votechat-backend/app/api/homeScreenAPI/createRoom/route.js
+++ votechat-backend/app/api/homeScreenAPI/createRoom/route.js
@@ -3,35 +3,24 @@ import Salas from "../../../../models/salas"
 import User from "../../../../models/user"
 import { NextResponse } from "next/server"
 import jwt from 'jsonwebtoken'
+import verifyToken from "../../verifyTokenFunction"
 
 export async function POST(req) {
-    let id_user;
-    let newUserToken;
-
-    await connectMongoDB()
-
     try {
-        //Verfica se recebeu o token e decide se decodifica ou cria um novo usuário
-        if(req.headers.get('authorization')){
-            // Decodifica o token
-            id_user = jwt.verify(req.headers.get('authorization'), process.env.SECRET_KEY).id_user
-        } else {
-            const createdUser = await User.create({})
-            id_user = createdUser._id
-            
-            // Se o token não existe, cria um novo
-            newUserToken = jwt.sign({ id_user }, process.env.SECRET_KEY)
-        }
+        //Acessa os dados da function verifyToken
+        const { id_user, token } = await verifyToken()
 
         //Acessa os dados do corpo da requisição
         const { roomName, roomDescription } = await req.json()
 
-        //Busca os dados no MongoDB
+        //Conecta ao MongoDB
+        await connectMongoDB()
 
+        //Cria o dado no MongoDB
         const createdRoom = await Salas.create({ name: roomName, description: roomDescription, members: { id_user: id_user } })
 
         //Devolve as informações como resposta da requisição
-        return NextResponse.json({ roomData: createdRoom, token: newUserToken, status: 200})
+        return NextResponse.json({ roomData: createdRoom, token: token, status: 200 })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ message: error, success: false });
